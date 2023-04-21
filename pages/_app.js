8d562cbc50718e9fecaab881b4ec1198b924a1fb
Nick Molina <104439965+nicholasmolina2019@users.noreply.github.com>
2023-04-21 02:42:54 -0500

    ready

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/react';
 import "../css/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
