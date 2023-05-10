c6a1d1276353f145d0f2f1e955dc2a9badae2158
Nick Molina <104439965+nicholasmolina2019@users.noreply.github.com>
2023-05-10 14:14:46 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300..700&family=Lora:wght@400;500;600&display=swap"
             rel="stylesheet"
           />
-         <meta property="og:image" content="https://i.ibb.co/cDRDh52/websitephoto.png" />
         </Head>
         <body>
           <Main />
