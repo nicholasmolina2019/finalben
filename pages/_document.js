3d5dc1a885bf77c5b3f2322858fc9f9823fbb6d4
Nick Molina <104439965+nicholasmolina2019@users.noreply.github.com>
2023-05-22 23:32:07 -0500

    tag
    google

--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,29 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-FHNJY22J82`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-FHNJY22J82');
+              `,
+            }}
+          />
+
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@300..700&family=Lora:wght@400;500;600&display=swap"
             rel="stylesheet"
           />
           <meta name="p:domain_verify" content="5d091b94430706ce79dfe57f8dda0bb0"/>
         </Head>
+        
         <body>
           <Main />
           <NextScript />
